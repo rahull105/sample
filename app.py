5afdd41534cf6ad85a02938c6cc1c411070fbd6c
rahull105 <1nt23cs105.kataru@nmit.ac.in>
2026-02-04 15:39:50 +0530

    updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from services.geoapify_service import get_nearby_places
+import os
 import math
 
 app = Flask(__name__)
@@ -55,4 +56,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
